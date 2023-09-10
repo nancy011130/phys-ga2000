01f56bbab8560238a99ff582261c07ae529461d1
nancy011130 <90431386+nancy011130@users.noreply.github.com>
2023-09-10 16:06:55 -0400

    Add files via upload

---
+++ ps-1/plot_gaussian.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x = np.arange(-10, 10, 0.01)
+
+mean = 0
+std = 3
+
+y = 1/(std*np.sqrt(2*np.pi)) * (np.exp(-0.5*((x-mean)/std)**2))
+plt.plot(x, y)
+plt.show
